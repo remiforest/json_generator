1c40edf4f5328d820eb2d914ca50f63a30ac5d46
remiforest <remi.forest@akilio.com>
2019-04-27 13:15:03 +0200

    refactor

--- generator.py
+++ generator.py
@@ -1,31 +1,79 @@
 #! /usr/bin/python
 
+"""
+Generate a JSON or CSV file of records based on a given schema
+
+"""
+
+
+
 import time
 import binascii
 import string
 import random
 import os
 import json
+import csv
 import sys
+
 
 from argparse import ArgumentParser
 
 parser = ArgumentParser(description='Generate random data in a json file. One record per line')
+parser.add_argument("number")
+parser.add_argument("outfile")
+parser.add_argument(
+    '--schema',
+    default="schema.json",
+    help='schema definition, default : schema.json'
+)
 
-last_names = json.load(open("last_name.json"))
+parser.add_argument(
+    '--format',
+    default="json",
+    help='output format, default : json'
+)
 
-def last_name():
-    return random.choice(last_names)["last"]
 
 
-def oid():
-    timestamp = '{0:x}'.format(int(time.time()))
-    rest = binascii.b2a_hex(os.urandom(8)).decode('ascii')
-    return timestamp + rest
+def create_folder_if_doesnt_exist(dirName):
+    try:
+        # Create target Directory
+        os.mkdir(dirName)
+    except :
+        pass
 
+
+out_folder = "output/"
+create_folder_if_doesnt_exist(out_folder)
+
+source_folder = "source/"
+create_folder_if_doesnt_exist(source_folder)
+
+
+#### Pre defined value types
+
+source_data = {}
+
+def load_source_data(source_name):
+    global source_data
+    print(source_name)
+    source_data[source_name] = [record[source_name] for record in json.load(open(source_folder + source_name + ".json"))]
+
+for source_name in ["email","first_name","last_name","country","card_number","card_vendor"]:
+    load_source_data(source_name)
+
+
+def get_value(value_type):
+    global source_data
+    return random.choice(source_data[value_type])
+
+
+#### Generated value types
 
 def custom_string(model_string):
-    output_string = ""
+    """ randomly generates a custom string based on a given model """
+    output_string = ''
     for c in model_string:
         if c == "n":
             output_string += str(random.randint(0,9))
@@ -39,66 +87,81 @@ def custom_string(model_string):
     return output_string
 
 
-def join(source_file,field):
-    f = open(source_file,"r")
-    json_object = json.loads(f.read())
-    yield json_object[field]
+
+#### JOIN management - used for having same values used in different files
+
+# def join(source_file,field):
+#     f = open(source_file,"r")
+#     json_object = json.loads(f.read())
+#     yield json_object[field]
 
 
+#### Field values generator 
 
-def generate_field(field_format):
+# Used to check value unicity
+given_values = {}
+MAX_RETRY = 1000 # max number of retry to generate an unique value
+
+def generate_field(field_name, field_format):
+    """ generate a value based on the schema definition """
+
+    global given_values
+
+    # Check if the field definition is a base type or a dictionary
     if isinstance(field_format, basestring):
-        if field_format == "OID":
-            return oid()
-        if field_format == "LAST":
-            return last_name()
-        else:
-            return custom_string(field_format)
+        if field_format in source_data:
+            new_value = get_value(field_format)
     if isinstance(field_format,dict):
-        if "FILE" in field_format:
-            file_name,field_name = field_format["FILE"].split(":")
-            join_file = open(file_name,"r")
-            json_object = json.loads(f.read())
-            return json_object[field_name]
+        if "custom" in field_format:
+            if "unique" in field_format:
+                unique = field_format["unique"]
+            else:
+                unique = False
+            number_of_try = 0
+            while True:
+                number_of_try += 1
+                new_value = custom_string(field_format["custom"])
+                if unique:
+                    if not field_name in given_values:
+                        given_values[field_name] = []
+                    if not new_value in given_values[field_name]:
+                        given_values[field_name].append(new_value)
+                        break
+                else:
+                    break
+                if number_of_try > MAX_RETRY:
+                    print("Failed to generate unique value after {} retries".format(MAX_RETRY))
+                    break
+
+    return new_value
 
 
-def main():
-    schema = json.load(open("schema.json"))
 
-    # check if FILE in schema
-    ext_file = False
-    ext_field = False
-    for k,v in schema.items():
-        if isinstance(v,dict):
-            if "FILE" in v:
-                ext_file,ext_field = v["FILE"].split(":")
+def main(number_of_records,outfile,schema,file_format):
 
-
-    number_of_records = int(sys.argv[1])
-    target_file = open(sys.argv[2],"w")
-
-    # get the list of all values for the given field
-    ext_vals = None
-    if ext_file:
-        ext_vals = []
-        a = json.load(open(ext_file))
-        for o in a:
-            ext_vals.append(o[ext_field])
+    print("Generating {} records with schema {} in file {}.{}".format(number_of_records,schema,outfile,file_format))
 
     new_dataset = []
     for i in range(number_of_records):
         new_doc = {}
         for field_name,field_format in schema.items():
-            if ext_vals and field_name == ext_field:
-                new_doc[field_name] = ext_vals[i%len(ext_vals)]
-            else:
-                new_doc[field_name] = generate_field(field_format)
+            new_doc[field_name] = generate_field(field_name,field_format)
         new_dataset.append(new_doc)
 
-    target_file.write(json.dumps(new_dataset))
-    target_file.close()
-
+    with open("{}.{}".format(out_folder + outfile,file_format),'w') as target_file:
+        if file_format == "json":
+            target_file.write(json.dumps(new_dataset))
+        elif file_format == "csv":
+            writer = csv.DictWriter(target_file, new_dataset[0].keys())
+            writer.writeheader()
+            writer.writerows(new_dataset)
+    
 
 if __name__ == '__main__':
-    main()
+    args = parser.parse_args()
+    number_of_records = int(args.number)
+    target_file = args.outfile
+    schema = json.load(open(args.schema))
+    file_format = args.format
+    main(number_of_records,target_file,schema,file_format)
 
