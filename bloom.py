0f39ee566c34ab44884565461894086b3803a3ec
remiforest <remi.forest@akilio.com>
2019-05-09 16:44:41 +0200

    added bloom.py adn requirements

---
+++ bloom.py
@@ -0,0 +1,87 @@
+import mmh3
+import math
+from bitarray import bitarray
+import sys
+
+class Error(Exception):
+    """Base class for exceptions."""
+    pass
+
+class FPError(Error):
+    """Exception raised for error in inputting the false positive.
+    Attributes:
+        message -- explanation of the error
+    """
+    def __init__(self, message):
+        self.message = message
+
+class Bloom:
+    """ A simple implementation of a bloom filter. 
+    Attributes:
+        n -- the expected number of elements to be added
+        to the filter. Used for number of bits allocated 
+        for the bitarray
+        fp -- the false positive value the user is willing
+        to tolerate.
+        k -- the number of desired hash functions
+    """
+
+    def __init__(self, n=0, fp=0, k=0):
+        try:
+            if fp > 0 and fp < 1:
+                self.fp = fp
+            else:
+                raise FPError('False Positive rate should be given as a decimal value between 0 and 1.')
+        except FPError as err:
+            print "FPError: %s" % err.message
+            sys.exit()
+        self.k = k
+        self.n = n
+        self.m = int(math.ceil(1 / (1 - (1 - self.fp ** (1 / float(self.k))) ** (1 / (float(self.k) * float(self.n))))))
+        self.eltsAdded = 0
+        self.bvector = bitarray(self.m)
+        self.bvector.setall(False)
+
+    # Calculates the current false positive probability
+    # based on the number of elements added so far
+    def get_fp_prob(self):
+        return (1 - math.exp(-self.k * self.eltsAdded / self.m))**self.k
+
+    # Calculates k number of hashes using
+    # the murmur hash function
+    def hash(self, string):
+        hash_arr = []
+        hash1 = mmh3.hash(string, 0)
+        hash2 = mmh3.hash(string, hash1)
+        for i in range(self.k):
+            hash_arr.append(abs((hash1 + i * hash2) % self.m))
+        return hash_arr
+
+    # Adds an element to the bloom filter
+    def add(self, string):
+        hashes = self.hash(string)
+        for elt in hashes:
+            self.bvector[elt] = True
+        self.eltsAdded += len(hashes)
+
+    # Queries for membership of element in the
+    # bloom filter
+    def query(self, string):
+        hashes = self.hash(string)
+        for elt in hashes:
+            if self.bvector[elt] != True:
+                return False
+        return True
+
+    # Clears the bloom filter and resets all
+    # values in the bit vector to 0
+    def clear(self):
+        self.bvector.setall(False)
+        self.eltsAdded = 0
+
+if __name__ == "__main__":
+    b_f = Bloom(100, .1, 5)
+    b_f.add("Matt")
+    b_f.add("Daria")
+    b_f.query("Matt")
+    b_f.query("KitKat")
